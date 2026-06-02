859916e6f7299e6f428b76bd58fa2eda72ac54be
MatheusHenriqueSouzaSantos <matheushenriquesouzasantos2@gmail.com>
2026-06-01 22:16:20 -0300

    feat:criando o service para criacao de logs do endereco

---
+++ Services/LogServices/ClienteLogService.cs
@@ -0,0 +1,93 @@
+﻿using ApiEstagioBicicletaria.Entities;
+using ApiEstagioBicicletaria.Entities.ClienteDomain;
+using ApiEstagioBicicletaria.Entities.UsuarioDomain;
+using ApiEstagioBicicletaria.Entities.VendedorDomain;
+using ApiEstagioBicicletaria.Repository.Repositorios;
+using System.Reflection;
+
+namespace ApiEstagioBicicletaria.Services.LogServices
+{
+    public class ClienteLogService
+    {
+        private readonly LogRepositorio<ClienteLog> _repositorio;
+
+        public ClienteLogService(LogRepositorio<ClienteLog> repositorio)
+        {
+            _repositorio = repositorio;
+        }
+
+        public void CriarLogsDeCriacaoClienteFisico(ClienteFisico cliente, Usuario usuarioResponsavel)
+        {
+            PropertyInfo[] propriedades = typeof(ClienteFisico).GetProperties();
+            foreach (PropertyInfo propriedade in propriedades)
+            {
+                var valorPropriedade = propriedade.GetValue(cliente);
+
+                ClienteLog log = new(
+                    cliente,
+                    TipoCliente.PessoaFisica,
+                    LogAcao.Criacao,
+                    propriedade.Name,
+                    null,
+                    valorPropriedade?.ToString(),
+                    usuarioResponsavel);
+
+                _repositorio.CriarLog(log);
+            }
+        }
+
+        public void CriarLogsDeCriacaoClienteJuridico(ClienteJuridico cliente, Usuario usuarioResponsavel)
+        {
+            PropertyInfo[] propriedades = typeof(ClienteJuridico).GetProperties();
+            foreach (PropertyInfo propriedade in propriedades)
+            {
+                var valorPropriedade = propriedade.GetValue(cliente);
+
+                ClienteLog log = new(
+                    cliente,
+                    TipoCliente.PessoaJuridica,
+                    LogAcao.Criacao,
+                    propriedade.Name,
+                    null,
+                    valorPropriedade?.ToString(),
+                    usuarioResponsavel);
+
+                _repositorio.CriarLog(log);
+            
+            }
+        }
+
+        public void CriarLogsDeAtualizacaoClienteFisico(ClienteFisico clienteAntigo, ClienteFisico clienteAtualizado,
+            Usuario usuarioResponsavel)
+        {
+            PropertyInfo[] propriedades = typeof(Clie).GetProperties();
+            foreach (PropertyInfo propriedade in propriedades)
+            {
+                if (Attribute.IsDefined(propriedade, typeof(AnotacaoDeAtributoASerIgnoradoLog)))
+                {
+                    continue;
+                }
+                var valorAntigoPropriedade = propriedade.GetValue(vendedorAntigo);
+                var valorAtualizadoPropriedade = propriedade.GetValue(vendedorAtualizado);
+
+                if (valorAntigoPropriedade != valorAtualizadoPropriedade)
+                {
+                    VendedorLog log = new(vendedorAtualizado,
+                    LogAcao.Atualizacao,
+                    propriedade.Name,
+                    valorAntigoPropriedade?.ToString(),
+                    valorAtualizadoPropriedade?.ToString(),
+                    usuarioResponsavel);
+
+                    _repositorio.CriarLog(log);
+                }
+
+            }
+        }
+
+        public void CriarLogsDeExclusao(Vendedor vendedor, Usuario usuarioResponsavel)
+        {
+            _repositorio.CriarLog(new VendedorLog(vendedor, LogAcao.Exclusao, "Ativo", true.ToString(), false.ToString(), usuarioResponsavel));
+        }
+    }
+}
---
+++ Services/LogServices/EnderecoLogService.cs
@@ -0,0 +1,75 @@
+﻿using ApiEstagioBicicletaria.Entities;
+using ApiEstagioBicicletaria.Entities.ClienteDomain;
+using ApiEstagioBicicletaria.Entities.ProdutoDomain;
+using ApiEstagioBicicletaria.Entities.UsuarioDomain;
+using ApiEstagioBicicletaria.Repository.Repositorios;
+using System.Reflection;
+
+namespace ApiEstagioBicicletaria.Services.LogServices
+{
+    public class EnderecoLogService
+    {
+        private readonly LogRepositorio<EnderecoLog> _repositorio;
+
+        public EnderecoLogService(LogRepositorio<EnderecoLog> repositorio)
+        {
+            _repositorio = repositorio;
+        }
+
+        public void CriarLogsDeCriacao(Endereco endereco,Cliente cliente, Usuario usuarioResponsavel)
+        {
+            PropertyInfo[] propriedades = typeof(Endereco).GetProperties();
+            foreach (PropertyInfo propriedade in propriedades)
+            {
+                var valorPropriedade = propriedade.GetValue(endereco);
+
+                EnderecoLog log = new(endereco,
+                    cliente,
+                    LogAcao.Criacao,
+                    propriedade.Name,
+                    null,
+                    valorPropriedade?.ToString(),
+                    usuarioResponsavel);
+
+                _repositorio.CriarLog(log);
+            }
+        }
+
+        public void CriarLogsDeAtualizacao(Endereco enderecoAntigo, Endereco enderecoAtualizado, Cliente cliente, 
+            Usuario usuarioResponsavel)
+        {
+            PropertyInfo[] propriedades = typeof(Endereco).GetProperties();
+            foreach (PropertyInfo propriedade in propriedades)
+            {
+                if (Attribute.IsDefined(propriedade, typeof(AnotacaoDeAtributoASerIgnoradoLog)))
+                {
+                    continue;
+                }
+                var valorAntigoPropriedade = propriedade.GetValue(enderecoAntigo);
+                var valorAtualizadoPropriedade = propriedade.GetValue(enderecoAtualizado);
+
+                if (valorAntigoPropriedade != valorAtualizadoPropriedade)
+                {
+                    EnderecoLog log = 
+                    new(enderecoAtualizado,
+                    cliente,
+                    LogAcao.Atualizacao,
+                    propriedade.Name,
+                    valorAntigoPropriedade?.ToString(),
+                    valorAtualizadoPropriedade?.ToString(),
+                    usuarioResponsavel);
+
+                    _repositorio.CriarLog(log);
+                }
+
+            }
+        }
+
+        public void CriarLogsDeExclusao(Endereco endereco, Cliente cliente, Usuario usuarioResponsavel)
+        {
+            EnderecoLog log = new EnderecoLog(endereco,cliente, LogAcao.Exclusao, "Ativo", true.ToString(), false.ToString(), usuarioResponsavel);
+
+            _repositorio.CriarLog(log);
+        }
+    }
+}
